63cba9b2c874a2c96054df6bd9d6db75b2fc52d8
zoeyzhong520 <zoeyzhong520@icloud.com>
2018-06-19 21:32:40 +0800

    init commit

---
+++ ChooseCityButtonView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChooseCityButtonView
+//
+//  Created by ZZJ on 2018/6/19.
+//  Copyright © 2018年 ZZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChooseCityButtonView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ChooseCityButtonView
+//
+//  Created by ZZJ on 2018/6/19.
+//  Copyright © 2018年 ZZJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
